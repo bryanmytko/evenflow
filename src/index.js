5366387cc31d968c539158f83fdbf94fe3767033
Bryan Mytko <bryanmytko@gmail.com>
2022-01-02 10:31:32 -0500

    Add materialize css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import axios from 'axios';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from './App';
-import './style.css';
-// import * as serviceWorker from './serviceWorker';
+import Login from './components/login.component';
 
-ReactDOM.render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/logout" element={<App />} />
-      </Routes>
-    </BrowserRouter>,
-  document.getElementById('app'));
+import 'materialize-css/dist/css/materialize.min.css'
+import './style.css';
+
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("app")
+);
