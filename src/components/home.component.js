5a5a26a17634abcafd0d7aed99174c2aa7e2fb47
Bryan Mytko <bryanmytko@gmail.com>
2022-01-03 04:50:59 -0500

    Wrap home content in card(s)

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -3,7 +3,13 @@ import React from 'react';
 import Nodes from './nodes.component';
 
 const Home = () => {
-  return <div class="card"><Nodes /></div>
+  return <div className="container">
+    <div className="row padding-20">
+      <div className="card node-card col s8 offset-s2">
+        <Nodes />
+      </div>
+    </div>
+  </div>
 };
 
 export default Home;
