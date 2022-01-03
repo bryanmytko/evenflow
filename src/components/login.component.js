fa48daedabb0f7a09aa66e5302af727f02600123
Bryan Mytko <bryanmytko@gmail.com>
2022-01-03 04:51:19 -0500

    Replace placeholder logo with text

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -17,7 +17,7 @@ const Login = () => {
     return <div className="container main-container">
       <div className="row">
         <div className="col s12 m8 l6 offset-m2 offset-l3">
-          <div className="logo"></div>
+          <div className="logo">evenflow.</div>
           <div className="card-panel">
             <div className="row no-margin-bottom">
               <form onSubmit={handleSubmit}>
