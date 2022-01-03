9fe991aabd65366ccbd37233b6e407c953bc8f5c
Bryan Mytko <bryanmytko@gmail.com>
2022-01-03 04:51:34 -0500

    Style "buttons"

--- src/components/nodes.component.js
+++ src/components/nodes.component.js
@@ -31,7 +31,7 @@ const Nodes = () => {
       <ul>
         {nodes.map(n => {
           if(!n.children || !n.children.length) return;
-          return <li key={n.title}><input type="button" value={n.title} onClick={() => follow(n._id)} /></li>
+          return <li key={n.title}><input className="btn" type="button" value={n.title} onClick={() => follow(n._id)} /></li>
         })}
       </ul>
     </>
@@ -42,7 +42,7 @@ const Nodes = () => {
       <ul>
         {nodes.map(n => {
           if(!n.children || !n.children.length) return;
-          return <li key={n.title}><input type="button" value={n.title} onClick={() => follow(n._id)} /></li>
+          return <li key={n.title}><input className="btn" type="button" value={n.title} onClick={() => follow(n._id)} /></li>
         })}
       </ul>
     </>
