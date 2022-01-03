4d26d63d6a3331d4f585efa692c47661398384cf
Bryan Mytko <bryanmytko@gmail.com>
2022-01-03 03:44:12 -0500

    Restructure HTML to have header/main/footer inside main div

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import App from './App';
+import AuthService from './services/auth.service';
 import Login from './components/login.component';
+import Header from './components/layout/header.component';
+import Footer from './components/layout/footer.component';
 
 import 'materialize-css/dist/css/materialize.min.css'
 import './style.css';
 
 render(
   <BrowserRouter>
-    <App />
+    <Header />
+    <main>
+      <App />
+    </main>
+    <Footer />
   </BrowserRouter>,
   document.getElementById("app")
 );
