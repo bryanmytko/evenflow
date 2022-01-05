4f6aa760a498eed787533ca02665d35a7aa15dcb
Bryan Mytko <bryanmytko@gmail.com>
2022-01-05 03:19:00 -0500

    Remove unecessary import statements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
-import AuthService from './services/auth.service';
-import Login from './components/login.component';
 import Header from './components/layout/header.component';
 import Footer from './components/layout/footer.component';
 
