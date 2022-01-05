6dcf5a9e6ceca4a7c05d5138b146f9301e333ff9
Bryan Mytko <bryanmytko@gmail.com>
2022-01-05 03:19:27 -0500

    Change UI to include creating nodes

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Nodes from './nodes.component';
 
@@ -6,7 +7,14 @@ const Home = () => {
   return <div className="container">
     <div className="row padding-20">
       <div className="card node-card col s8 offset-s2">
+        <h5>Charts</h5>
         <Nodes />
+      </div>
+      <div className="card node-card col s8 offset-s2">
+        <h5>Add Chart</h5>
+        <p>
+          <Link to="/createNode" className="btn">Create</Link>
+        </p>
       </div>
     </div>
   </div>
