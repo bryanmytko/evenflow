bbbbf611fbd040229dc25f13ec64c66dfe9f3e9b
Bryan Mytko <bryanmytko@gmail.com>
2022-01-05 03:18:27 -0500

    Add createNode component

---
+++ src/components/createNode.component.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useState } from 'react';
+import UserService from '../services/user.service';
+
+const createNode = () => {
+  const [title, setTitle] = useState('');
+
+  const createChart = () => {
+    UserService.createNode({ title });
+    // @TODO update UI with success / error
+  }
+
+  return <div className="container col s8 offset-s2">
+    <h5>Create Chart</h5>
+    <input placeholder="Title"
+      value={title}
+      onChange={e => setTitle(e.target.value)} />
+    <button className="btn"
+      onClick={createChart}>Save</button>
+  </div>
+};
+
+export default createNode;
