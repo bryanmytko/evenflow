9e8b0f22295de2fab0664a0319fa165a8932da2c
Bryan Mytko <bryanmytko@gmail.com>
2022-01-14 18:02:00 -0500

    WIP Add Child Node creation with real data

--- src/components/chartCreate/index.js
+++ src/components/chartCreate/index.js
@@ -6,171 +6,77 @@ import UserService from '../../services/user.service';
 
 import './style.css';
 
-const dummyData = { nodes: [{
-  title: "Grandpa",
-  id: "1",
-  children: [
-    {
-      title: "Mom",
-      id: "2",
-      children: [{
-        title: "Bryan",
-        id: "4",
-        children: []
-      },
-      {
-        title: "Erica",
-        id: "5",
-        children: [{
-          title: "Grandkid",
-          id: "7",
-          children: []
-        }]
-      }]
-    },
-    {
-      title: "Aunt",
-      id: "3",
-      children: [
-        {
-          title: "Cousin",
-          id: "6",
-          children: []
-        }
-      ]
-    }
-  ]
-}]};
-
-const findById = (data, id, childNode) => {
+const insertChildNode = (data, id, childNode) => {
   const iter = (a) => {
-    if (a._id === id) {
+    if (a.id === id) {
       a.children ? a.children.push(childNode) : a.children = [childNode];
       return true;
     }
     return Array.isArray(a.children) && a.children.some(iter);
   }
 
-  let result;
   data.some(iter);
   return data;
-}
-
-const ChartCreate = () => {
-
-  /* This is the basic pattern we are basing this all on */
-  const returnChildren = (child) => {
-    return <>
-      <div className="indented">{child.title}
-        {(child.children || []).map((child, index) => returnChildren(child))}
-      </div>
-    </>
-  }
-  return <>
-    <h5>{dummyData.nodes[0].title}</h5>
-    {(dummyData.nodes[0].children || []).map((child, index) => returnChildren(child))}
-  </>;
-
-
-  // const nestedNodes = (parent.children || []).map((child, index) => {
-  //   return <>
-  //     <p key={index}>{child.title}</p>
-  //     <ChartCreateChild />
-  //   </>;
-  // });
-
-//   const [title, setTitle] = useState('');
-//   const [parentCreated, setParentCreated] = useState(false);
-//   const [chartData, setChartData] = useState({ nodes: [] });
-//   const [workingChild, setWorkingChild] = useState({ title: '' });
-//
-//   const createParent = async () => {
-//     /* @TODO Error handling */
-//     const response = await UserService.createNode({ title });
-//     const { node: parent } = response.data;
-//
-//     setChartData({ nodes: [...chartData.nodes, parent] });
-//     setParentCreated(true);
-//   };
-//
-//   const createChild = async () => {
-//     const response = await UserService.createNode({
-//       title: workingChild.title,
-//       parentId: workingChild.parentId
-//     });
-//     const { node: childNode } = response.data;
-//
-//     const newStuff = findById(chartData.nodes, workingChild.parentId, childNode);
-// console.log(newStuff)
-//     setChartData({ nodes: newStuff });
-//   };
-
-
-
-
-
-  // const foofoo = (node) => {
-  //   return <ChartCreateChild key={node._id}
-  //     parent={node}
-  //     workingChild={workingChild}
-  //     setWorkingChild={setWorkingChild}
-  //     createChild={createChild} />
-  // }
-  //
-  // if(parentCreated) {
-    // const nestedNodes = (parent.children || []).map((child, index) => {
-    //   return <>
-    //     <p key={index}>{child.title}</p>
-    //     <ChartCreateChild />
-    //   </>;
-    // });
-// {//nestedNodes}
-  //   return <>
-  //     { chartData.nodes.map(node => foofoo(node)) }
-  //   </>;
-  // }
-
-  // return <div style={{marginLeft: "30px"}}>
-  //   <input placeholder="Title"
-  //     value={title}
-  //     onChange={e => setTitle(e.target.value)} />
-  //   <button className="btn"
-  //     onClick={createParent}>Save</button>
-  // </div>;
 };
 
-  // const [parent, setParent] = useState({ title: '', id: '', saved: false });
-  // const [children, setChildren] = useState([]);
-  //
-  // const createChart = async () => {
-  //   const response = await UserService.createNode({ title: parent.title });
-  //   setParent({ ...parent, id: response.data.node._id, saved: true });
-  // }
-  //
-  // const addChildForm = () => {
-  //   setChildren([...children, children.length + 1]);
-  // }
-  //
-  // return <div className="chart-create-container container">
-  //   <div className="row">
-  //     <div className="col s8 offset-s2">
-  //       <h5>Create Chart</h5>
-  //
-  //       <p className={parent.saved ? '' : 'hide'}>{parent.title}</p>
-  //
-  //       <input placeholder="Title"
-  //         className={parent.saved ? 'hide' : ''}
-  //         value={parent.title}
-  //         onChange={e => setParent({ title: e.target.value })} />
-  //       <button className={`btn ${parent.saved ? 'hide' : ''}`}
-  //         onClick={createChart}>Save</button>
-  //
-  //       <button className={`btn ${parent.saved ? '' : 'hide'}`}
-  //         onClick={addChildForm}>Add Child</button>
-  //
-  //       {children.map((n, index) => <ChartCreateChild key={index} parent={parent} addChildForm={addChildForm} setParent={setParent} />)}
-  //     </div>
-  //   </div>
-  // </div>
+const ChartCreate = () => {
+  /* @TODO consider a reducer: ADD_CHILD, CREATE_PARENT, etc., */
+  const [chartData, setChartData] = useState({});
+  const [workingChild, setWorkingChild] = useState({ title: '', id: '' });
+  const [parentId, setParentId] = useState('');
+  const [parent, setParent] = useState({ title: '' });
+  const [showForm, setShowForm] = useState(false);
+
+  const addChildNode = () => {
+    const newData = insertChildNode(chartData.nodes, parentId, workingChild);
+    setChartData({ nodes: newData });
+    setShowForm(false);
+    setWorkingChild({ title: '' });
+  };
+
+  const initializeForm = (child) => {
+    setShowForm(!showForm);
+    setParentId(child.id);
+  };
+
+  const createParent = async () => {
+    const response = await UserService.createNode({ title: parent.title });
+    const { node } = response.data;
+    const parentData = { title: parent.title, id: node._id, children: [] };
+
+    setChartData({ nodes: [parentData]});
+    setParent(parentData);
+  };
+
+  const returnChildren = (child) => {
+    return <div key={child.id} className="create-node-unit">
+      <label>{child.title}</label>
+      <button className="btn" onClick={() => initializeForm(child)}>+</button>
+      {(child.children || []).map((child, index) => returnChildren(child))}
+    </div>
+  }
+
+  if(!parent.id){
+    return <div className="chart-create-container container">
+      <label>Chart Name:</label>
+      <input value={parent.title} onChange={e => setParent({ title: e.target.value })} />
+      <button className="btn" onClick={createParent}>Save</button>
+    </div>;
+  }
+
+  return <div className="chart-create-container container">
+    <div className="create-node-unit">
+      <label className="title">{parent.title}</label>
+      <button className="btn" onClick={() => initializeForm(parent)}>+</button>
+      {(parent.children || []).map((child, index) => returnChildren(child))}
+
+      <div className={`chart-create-control card green lighten-2 ${(showForm ? '' : 'hide')}`}>
+        <input value={workingChild.title}
+          onChange={e => setWorkingChild({ title: e.target.value, id: Math.floor(Math.random()*100) })} />
+        <button className="btn" onClick={addChildNode}>SAVE</button>
+      </div>
+    </div>
+  </div>;
+};
 
 export default ChartCreate;
