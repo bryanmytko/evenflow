6d35db91852505d22e7ccf4f49e4495ce09959fe
Bryan Mytko <bryanmytko@gmail.com>
2022-01-14 01:03:02 -0500

    WIP basic node recursion pattern with dummy data sketched up

--- src/components/chartCreate/chartCreateChild/index.js
+++ src/components/chartCreate/chartCreateChild/index.js
@@ -3,16 +3,12 @@ import React, { useState} from 'react';
 import UserService from '../../../services/user.service';
 
 const ChartCreateChild = (props) => {
-  const nestedNodes = (props.parent.children || []).map((child, index) => {
-    return <p key={index}>{child.title}</p>;
-  });
 
   return <div>
     <h5>{props.parent.title}</h5>
     <input placeholder="Child Title" value={props.workingChild.title}
-    onChange={e => props.setWorkingChild({ title: e.target.value, parentId: props.parent._id }) } />
+      onChange={e => props.setWorkingChild({ title: e.target.value, parentId: props.parent._id }) } />
     <button className="btn" onClick={props.createChild}>Add Child</button>
-    {nestedNodes}
   </div>
 };
 
--- src/components/chartCreate/index.js
+++ src/components/chartCreate/index.js
@@ -6,48 +6,137 @@ import UserService from '../../services/user.service';
 
 import './style.css';
 
-const ChartCreate = () => {
-  const [title, setTitle] = useState('');
-  const [parentCreated, setParentCreated] = useState(false);
-  const [chartData, setChartData] = useState({ nodes: [] });
-  const [workingChild, setWorkingChild] = useState({ title: '' });
+const dummyData = { nodes: [{
+  title: "Grandpa",
+  id: "1",
+  children: [
+    {
+      title: "Mom",
+      id: "2",
+      children: [{
+        title: "Bryan",
+        id: "4",
+        children: []
+      },
+      {
+        title: "Erica",
+        id: "5",
+        children: [{
+          title: "Grandkid",
+          id: "7",
+          children: []
+        }]
+      }]
+    },
+    {
+      title: "Aunt",
+      id: "3",
+      children: [
+        {
+          title: "Cousin",
+          id: "6",
+          children: []
+        }
+      ]
+    }
+  ]
+}]};
 
-  const createParent = async () => {
-    /* @TODO Error handling */
-    const response = await UserService.createNode({ title });
-    const { node: parent } = response.data;
-
-    setChartData({ nodes: [...chartData.nodes, parent] });
-    setParentCreated(true);
-  };
-
-  const createChild = async () => {
-    const response = await UserService.createNode({
-      title: workingChild.title,
-      parentId: workingChild.parentId
-    });
-    const { node: childNode } = response.data;
-
-    setChartData({ nodes: [...chartData.nodes, childNode] });
-  };
-
-  if(parentCreated) {
-    return <>
-      { chartData.nodes.map(node => <ChartCreateChild key={node._id}
-        parent={node}
-        workingChild={workingChild}
-        setWorkingChild={setWorkingChild}
-        createChild={createChild} />) }
-    </>;
+const findById = (data, id, childNode) => {
+  const iter = (a) => {
+    if (a._id === id) {
+      a.children ? a.children.push(childNode) : a.children = [childNode];
+      return true;
+    }
+    return Array.isArray(a.children) && a.children.some(iter);
   }
 
-  return <div style={{marginLeft: "30px"}}>
-    <input placeholder="Title"
-      value={title}
-      onChange={e => setTitle(e.target.value)} />
-    <button className="btn"
-      onClick={createParent}>Save</button>
-  </div>;
+  let result;
+  data.some(iter);
+  return data;
+}
+
+const ChartCreate = () => {
+
+  /* This is the basic pattern we are basing this all on */
+  const returnChildren = (child) => {
+    return <>
+      <div className="indented">{child.title}
+        {(child.children || []).map((child, index) => returnChildren(child))}
+      </div>
+    </>
+  }
+  return <>
+    <h5>{dummyData.nodes[0].title}</h5>
+    {(dummyData.nodes[0].children || []).map((child, index) => returnChildren(child))}
+  </>;
+
+
+  // const nestedNodes = (parent.children || []).map((child, index) => {
+  //   return <>
+  //     <p key={index}>{child.title}</p>
+  //     <ChartCreateChild />
+  //   </>;
+  // });
+
+//   const [title, setTitle] = useState('');
+//   const [parentCreated, setParentCreated] = useState(false);
+//   const [chartData, setChartData] = useState({ nodes: [] });
+//   const [workingChild, setWorkingChild] = useState({ title: '' });
+//
+//   const createParent = async () => {
+//     /* @TODO Error handling */
+//     const response = await UserService.createNode({ title });
+//     const { node: parent } = response.data;
+//
+//     setChartData({ nodes: [...chartData.nodes, parent] });
+//     setParentCreated(true);
+//   };
+//
+//   const createChild = async () => {
+//     const response = await UserService.createNode({
+//       title: workingChild.title,
+//       parentId: workingChild.parentId
+//     });
+//     const { node: childNode } = response.data;
+//
+//     const newStuff = findById(chartData.nodes, workingChild.parentId, childNode);
+// console.log(newStuff)
+//     setChartData({ nodes: newStuff });
+//   };
+
+
+
+
+
+  // const foofoo = (node) => {
+  //   return <ChartCreateChild key={node._id}
+  //     parent={node}
+  //     workingChild={workingChild}
+  //     setWorkingChild={setWorkingChild}
+  //     createChild={createChild} />
+  // }
+  //
+  // if(parentCreated) {
+    // const nestedNodes = (parent.children || []).map((child, index) => {
+    //   return <>
+    //     <p key={index}>{child.title}</p>
+    //     <ChartCreateChild />
+    //   </>;
+    // });
+// {//nestedNodes}
+  //   return <>
+  //     { chartData.nodes.map(node => foofoo(node)) }
+  //   </>;
+  // }
+
+  // return <div style={{marginLeft: "30px"}}>
+  //   <input placeholder="Title"
+  //     value={title}
+  //     onChange={e => setTitle(e.target.value)} />
+  //   <button className="btn"
+  //     onClick={createParent}>Save</button>
+  // </div>;
 };
 
   // const [parent, setParent] = useState({ title: '', id: '', saved: false });
