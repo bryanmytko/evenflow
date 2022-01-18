3ca942bd10b8363f2322635d7695f75e52f981ca
Bryan Mytko <bryanmytko@gmail.com>
2022-01-17 19:45:25 -0500

    Restyle the home screen

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -6,20 +6,20 @@ import { ChartList } from '../';
 import './style.css';
 
 const Home = () => {
-  return <div className="home container">
-    <div className="row padding-20">
-      <div className="card node-card col s8 offset-s2">
-        <h5>Charts</h5>
-        <ChartList />
-      </div>
-      <div className="card node-card col s8 offset-s2">
-        <h5>Add Chart</h5>
-        <p>
-          <Link to="/chart/create" className="btn">Create</Link>
-        </p>
-      </div>
+  return <div className="node-card">
+    <a href="/">
+      <div className="logo"></div>
+    </a>
+    <h5>My Charts</h5>
+    <div className="card node-card col s8 offset-s2">
+      <ChartList />
     </div>
-  </div>
+    <p>
+      <Link to="/chart/create" className="btn btn-new">+ Create New Chart</Link>
+      &nbsp; 
+      <Link to="/logout" className="btn btn-new">Logout</Link>
+    </p>
+  </div>;
 };
 
 export default Home;
