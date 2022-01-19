40ccc20e9600c441634650cb7826171407ebe7c2
Bryan Mytko <bryanmytko@gmail.com>
2022-01-19 05:02:28 -0500

    Add .replaceChildNode

--- src/services/object.service.js
+++ src/services/object.service.js
@@ -1,15 +1,27 @@
 class ObjectService {
   insertChildNode = (data, id, childNode) => {
-    const iter = (a) => {
+    const iter = a => {
       if (a.id === id) {
         a.children ? a.children.push(childNode) : a.children = [childNode];
         return true;
       }
       return Array.isArray(a.children) && a.children.some(iter);
-    }
+    };
 
     data.some(iter);
     return data;
+  }
+
+  replaceChildNode = (data, id, childNode) => {
+    const iter = a => {
+      if (a._id === id) {
+        a = { ...a, ...childNode };
+        return a
+      }
+      if(a.children.length) return { ...a, children: a.children.map(iter) }; //.some(iter);
+    };
+
+    return data.map(iter)[0];
   }
 }
 
