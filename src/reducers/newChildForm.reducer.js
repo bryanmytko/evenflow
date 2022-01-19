fffd0c4d8bb2d70c81eb127723c14ba84b504e44
Bryan Mytko <bryanmytko@gmail.com>
2022-01-19 02:43:09 -0500

    Add EDIT_CHILD action to reducer

--- src/reducers/newChildForm.reducer.js
+++ src/reducers/newChildForm.reducer.js
@@ -1,10 +1,21 @@
 const NewChildFormReducer = (state, action) => {
+  let toggle;
+
   switch(action.type) {
     case 'HIDDEN':
       return { ...state, hidden: true, terminating: false, formData: { title: '', payload: '' }};
     case 'NEW_CHILD':
-      const toggle = !state.hidden;
+      toggle = !state.hidden;
       return { ...state, hidden: toggle, parentId: action.parentId, terminating: false };
+    case 'EDIT_CHILD':
+      toggle = !state.hidden;
+      const terminating = action.formData.payload === '' ? false : true;
+      return { ...state,
+        hidden: toggle,
+        parentId: action.parentId,
+        terminating,
+        formData: { title: action.formData.title, payload: action.formData.payload }
+      };
     case 'TERMINATING':
       return { ...state, hidden: false, terminating: !state.terminating };
     case 'VALUE_CHANGE':
