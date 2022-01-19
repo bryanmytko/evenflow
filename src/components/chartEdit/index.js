3a09bad81a09d326ac47c0c0062812bc8aa0d553
Bryan Mytko <bryanmytko@gmail.com>
2022-01-19 02:42:33 -0500

    WIP add edit chart component

--- src/components/chartEdit/index.js
+++ src/components/chartEdit/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { Logo } from '../';
+import { ChartCreateForm,Logo } from '../';
 import { UserService } from '../../services';
+import { NewChildFormReducer } from '../../reducers';
+
+import './style.css';
+
+const initialState = {
+  hidden: true,
+  terminating: false,
+  parentId: '',
+  formData: {
+    title: '',
+    payload: ''
+  }
+};
 
 const ChartEdit = () => {
   const [tree, setTree] = useState([]);
+  const [position, setPosition] = useState({ x: 0, y: 200 });
+  const [state, dispatch] = useReducer(NewChildFormReducer, initialState);
+
   const params = useParams();
   const { id } = params;
 
@@ -16,9 +32,32 @@ const ChartEdit = () => {
     })
   }, [id]);
 
+  const updateChild = async () => {
+    const { title, payload } = state.formData;
+    const { parentId } = state;
+    const response = await UserService.createNode({ title, payload, parentId });
+    const childData = { title, payload, id: response.data.node._id };
+
+    // ObjectService.insertChildNode([tree], parentId, childData);
+    dispatch({ type: 'HIDDEN' });
+  };
+
+  const toggleModal = (e, child) => {
+    console.log(child.title)
+    setPosition({ x: e.pageX, y: e.pageY });
+    dispatch({
+      type: 'EDIT_CHILD',
+      parentId: child.id,
+      formData: { title: child.title, payload: child.payload }
+    });
+  }
+
   const showChildren = (child, index) => {
     return <li key={index}>
       <span>{child.title}
+        <button className={`btn ${child.payload ? 'hide' : ''}`}
+          id={`button-${index}`}
+          onClick={(e) => toggleModal(e, child)}>+</button>
         <span className="payload">{child.payload}</span>
       </span>
       <ul>
@@ -33,6 +72,12 @@ const ChartEdit = () => {
       <ul className="wtree">
         {(tree.children || []).map((child, index) => showChildren(child, index))}
       </ul>
+      <ChartCreateForm
+        position={position}
+        action={updateChild}
+        dispatch={dispatch}
+        state={state}
+      />
     </>;
   }
 
