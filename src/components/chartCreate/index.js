af5df3cf5e833fdc91741b0da61c6c00f9889468
Bryan Mytko <bryanmytko@gmail.com>
2022-01-20 20:29:36 -0500

    Migrate all "create" logic to chartEdit except root tree creation

--- src/components/chartCreate/index.js
+++ src/components/chartCreate/index.js
@@ -1,92 +1,28 @@
-import React from 'react';
-import { useReducer, useState } from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { ChartCreateForm, Expandable, Logo } from '../';
-import { UserService, ObjectService } from '../../services';
-import { NewChildFormReducer } from '../../reducers';
+import { Logo } from '../';
+import { UserService } from '../../services';
 
 import './style.css';
 
-const initialState = {
-  hidden: true,
-  terminating: false,
-  parentId: '',
-  formData: {
-    title: '',
-    payload: ''
-  }
-};
-
 const ChartCreate = () => {
-  const [tree, setTree] = useState({ title: '' });
-  const [position, setPosition] = useState({ x: 0, y: 200 });
-  const [state, dispatch] = useReducer(NewChildFormReducer, initialState);
-
-  const createChild = async () => {
-    const { title, payload } = state.formData;
-    const { parentId } = state;
-    const response = await UserService.createNode({ title, payload, parentId });
-    const childData = { title, payload, id: response.data.node._id };
-
-    ObjectService.insertChildNode(tree, parentId, childData);
-    dispatch({ type: 'HIDDEN' });
-  };
+  const [title, setTitle] = useState('');
+  const navigate = useNavigate();
 
   const createTreeRoot = async () => {
-    const response = await UserService.createNode({ title: tree.title, payload: '' });
+    const response = await UserService.createNode({ title, payload: '' });
     const { node } = response.data;
-    const data = { title: tree.title, id: node._id, payload: '', children: [] };
-
-    setTree(data);
+    navigate(`/chart/edit/${node._id}`);
   };
-
-  const toggleModal = (e, child) => {
-    setPosition({ x: e.pageX, y: e.pageY });
-    dispatch({ type: 'NEW_CHILD', parentId: child.id });
-  }
-
-  const showChildren = (child, index) => {
-    return <li key={index}>
-      <span>{child.title}
-      <button className={`btn ${child.payload ? 'hide' : ''}`}
-        id={`button-${index}`}
-        onClick={(e) => toggleModal(e, child)}>+</button>
-        <Expandable content={child.payload} />
-      </span>
-      <ul>
-        {(child.children || []).map((child, index) => showChildren(child, index))}
-      </ul>
-    </li>;
-  };
-
-  const showContent = () => {
-    if(!tree.id){
-      return <div className="chart-create-form card chart-create-initialize">
-        <label>Chart Name:</label>
-        <input value={tree.title} onChange={e => setTree({ title: e.target.value })} />
-        <button className="btn" onClick={createTreeRoot}>Save</button>
-      </div>;
-    }
-
-    return <>
-      <h5>{tree.title}
-        <button className="btn" onClick={(e) => toggleModal(e, tree)}>+</button>
-      </h5>
-      <ul className="wtree">
-        {(tree.children || []).map((child, index) => showChildren(child, index))}
-      </ul>
-      <ChartCreateForm
-        action={createChild}
-        dispatch={dispatch}
-        state={state}
-        position={position}
-      />
-    </>;
-  }
 
   return <div className="node-card">
     <Logo />
-   {showContent()}
+    <div className="chart-create-form card chart-create-initialize">
+      <label>Chart Name:</label>
+      <input value={title} onChange={e => setTitle(e.target.value)} />
+      <button className="btn" onClick={createTreeRoot}>Save</button>
+    </div>
   </div>;
 };
 
