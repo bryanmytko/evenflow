7547a5446d9939d4c57c8fedeeff02368e5d1592
Bryan Mytko <bryanmytko@gmail.com>
2022-01-24 01:51:22 -0500

    Export ValidationService

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,4 @@
 export { default as AuthService } from './auth.service';
 export { default as ObjectService } from './object.service';
 export { default as UserService } from './user.service';
+export { default as ValidationService } from './validation.service';
