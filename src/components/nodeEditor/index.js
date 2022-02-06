84fd9b5fe87f1d0f801185e9c32773dde7c30ed7
Bryan Mytko <bryanmytko@gmail.com>
2022-02-06 09:30:13 -0500

    WIP add bold button

--- src/components/nodeEditor/index.js
+++ src/components/nodeEditor/index.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 import 'draft-js/dist/Draft.css';
 
 import './style.css';
@@ -10,19 +10,35 @@ const NodeEditor = () => {
   );
 
   const editor = useRef(null);
+
   function focusEditor() {
     editor.current.focus();
   }
 
+  const handleKeyCommand = (command, editorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+
+    if(newState) {
+      setEditorState(newState);
+      return 'handled';
+    }
+
+    return 'not-handled';
+  }
+
+  const onBoldClick = () => setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
+
   return (
     <div
-      style={{ border: "1px solid black", minHeight: "6em", cursor: "text" }}
+      style={{ border: "1px solid black", minHeight: "6em", cursor: "text", backgroundColor: "white" }}
       onClick={focusEditor}
     >
+      {/* <button onClick={onBoldClick}>Bold</button> */}
       <Editor
         ref={editor}
         editorState={editorState}
         onChange={setEditorState}
+        handleKeyCommand={handleKeyCommand}
         placeholder="Write something!"
       />
     </div>
