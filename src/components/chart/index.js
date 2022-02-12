5347aac8cdc911a11b1f0a508cfe06eed382eb5e
Bryan Mytko <bryanmytko@gmail.com>
2022-02-11 23:44:05 -0500

    Add read-only Editor for viewing payloads

--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import {
+  Editor,
+  EditorState,
+  ContentState,
+  convertFromRaw,
+} from 'draft-js';
 
 import { Breadcrumbs, Logo } from '../';
 import { AuthService, UserService } from '../../services';
 
 const Chart = () => {
-  const [chart, setChart] = useState([]);
+  const [chart, setChart] = useState('');
   const [children, setChildren] = useState([]);
+  const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
   const [breadcrumbs, setBreadcrumbs] = useState([]);
   const params = useParams();
   const navigate = useNavigate();
@@ -14,12 +21,25 @@ const Chart = () => {
   useEffect(() => {
     (async () => {
       const response = await UserService.getNodeSlug(params.slug);
+
       if(Object.keys(response.data).length === 0) return navigate('/login');
       if(!AuthService.currentUser() && response.data.node.private) return navigate('/login');
+
       setChart(response.data.node);
       setChildren(response.data.node.children);
     })();
   }, [params.slug, navigate]);
+
+  useEffect(() => {
+    try {
+      if(!chart.payload) return setEditorState(EditorState.createEmpty());
+      const parsedContent = JSON.parse(chart.payload);
+      setEditorState(EditorState.createWithContent(convertFromRaw(parsedContent)));
+    } catch(err) {
+      /* This is gross but we need to support legacy content which was a raw string */
+      setEditorState(EditorState.createWithContent(ContentState.createFromText(chart.payload)));
+    }
+  }, [chart.payload]);
 
   useEffect(() => {
     setBreadcrumbs(JSON.parse(window.localStorage.getItem('breadcrumbs')) || []);
@@ -40,7 +60,7 @@ const Chart = () => {
     const len = breadcrumbs.indexOf(current);
     setBreadcrumbs([...breadcrumbs.slice(0, len)]);
     return navigate(`/chart/${current.slug}`, { state: breadcrumbs });
-  }
+  };
 
   const content = () => {
     if(children.length){
@@ -52,7 +72,7 @@ const Chart = () => {
        })}
      </ul>;
     }
-    return <p className="payload">{chart.payload}</p>;
+    return <div className="editor-payload"><Editor readOnly={true} editorState={editorState} /></div>;
   }
 
   return <div className="main-container container">
